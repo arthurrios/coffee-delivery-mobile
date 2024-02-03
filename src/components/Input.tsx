d73d67c0e115f25c9afa7cbacde45525ccaacd6c
arthur <arthur.rios007@gmail.com>
2024-02-03 15:38:04 -0300

    fix: input icon dynamic style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,29 @@
 import { Input, InputField, InputIcon, InputSlot } from '@gluestack-ui/themed'
 import { MagnifyingGlass } from 'phosphor-react-native'
+import { useState } from 'react'
 
 type Props = {
   placeholder: string
 }
 
 export function RootInput({ placeholder, ...props }: Props) {
+  const [isFocused, setIsFocused] = useState<boolean>(false)
+  const [inputValue, setInputValue] = useState('')
+
+  const isFocusedAndTyped = isFocused && inputValue !== ''
+
+  const handleFocus = () => {
+    setIsFocused(true)
+  }
+
+  const handleBlur = () => {
+    setIsFocused(false)
+  }
+
+  const handleInputChange = (text: string) => {
+    setInputValue(text)
+  }
+
   return (
     <Input
       borderRadius="$md"
@@ -15,13 +33,26 @@ export function RootInput({ placeholder, ...props }: Props) {
       bgColor="$gray_200"
     >
       <InputSlot>
-        <InputIcon as={MagnifyingGlass} color="$gray_400" />
+        <InputIcon
+          as={MagnifyingGlass}
+          color={
+            isFocusedAndTyped
+              ? '$yellow_dark'
+              : isFocused
+                ? '$yellow'
+                : '$gray_400'
+          }
+        />
       </InputSlot>
       <InputField
         fontFamily="$body"
         placeholder={placeholder}
         placeholderTextColor="$gray_400"
         color="#FFFFFF"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onChangeText={handleInputChange}
+        value={inputValue}
       />
     </Input>
   )
