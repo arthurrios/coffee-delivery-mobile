50e4394bb3abdfba2a25bd1843dd31c067cb54c0
arthur <arthur.rios007@gmail.com>
2024-02-03 17:34:59 -0300

    feat: add navigation

--- App.tsx
+++ App.tsx
@@ -3,12 +3,9 @@ import { GluestackUIProvider } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Baloo2_700Bold, useFonts } from '@expo-google-fonts/baloo-2'
 import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
-import { Splash } from '@screens/Splash'
-import { Catalog } from '@screens/Catalog'
-import { Product } from '@screens/Product'
 import { Loading } from '@components/Loading'
-import { Cart } from '@screens/Cart'
 import { OrderConfirmed } from '@screens/OrderConfirmed'
+import { Routes } from '@routes/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,7 +16,7 @@ export default function App() {
 
   return (
     <GluestackUIProvider config={config}>
-      {fontsLoaded ? <OrderConfirmed /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   )
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,40 @@
+import { CoffeeDTO } from '@dtos/CoffeeDTO'
+import { Box } from '@gluestack-ui/themed'
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { Cart } from '@screens/Cart'
+import { Catalog } from '@screens/Catalog'
+import { OrderConfirmed } from '@screens/OrderConfirmed'
+import { Product } from '@screens/Product'
+import { Splash } from '@screens/Splash'
+
+type AppRoutes = {
+  catalog: undefined
+  splash: undefined
+  product: CoffeeDTO
+  cart: undefined
+  orderConfirmed: undefined
+}
+
+export type AppNavigationRoutesProps = StackNavigationProp<AppRoutes>
+
+const { Navigator, Screen } = createStackNavigator()
+
+export function Routes() {
+  return (
+    <Box flex={1} bgColor="$gray_900">
+      <NavigationContainer>
+        <Navigator screenOptions={{ headerShown: false }}>
+          <Screen name="catalog" component={Catalog} />
+          <Screen name="splash" component={Splash} />
+          <Screen name="product" component={Product} />
+          <Screen name="cart" component={Cart} />
+          <Screen name="orderConfirmed" component={OrderConfirmed} />
+        </Navigator>
+      </NavigationContainer>
+    </Box>
+  )
+}
