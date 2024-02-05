34d9998e815108ae8905386bdeba08263a7d36cf
arthur <arthur.rios007@gmail.com>
2024-02-05 19:57:20 -0300

    chore: change storage to mmkv

--- src/storage/storageCart.ts
+++ src/storage/storageCart.ts
@@ -1,68 +1,83 @@
-/* eslint-disable prefer-const */
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { MMKV } from 'react-native-mmkv'
+
+export const storage = new MMKV()
 
 const CART_STORAGE = '@COFFEEDELIVERY_CART'
 
-/* eslint-disable no-useless-catch */
 export type StorageCartProps = {
   id: string
   name: string
   size: string
   quantity: number
   image: string
+  price: number
 }
 
-export async function storageProductGetAll() {
-  try {
-    const storage = await AsyncStorage.getItem(CART_STORAGE)
-    const products: StorageCartProps[] = storage ? JSON.parse(storage) : []
+export function storageProductGetAll() {
+  const storageItems = storage.getString(CART_STORAGE) || ''
+  const products: StorageCartProps[] = storageItems
+    ? JSON.parse(storageItems)
+    : []
 
-    return products
-  } catch (error) {
-    throw error
+  return products
+}
+
+export function storageProductSave(newProduct: StorageCartProps) {
+  let products = storageProductGetAll()
+
+  const productExists = products.filter(
+    (product) => product.id === newProduct.id,
+  )
+
+  if (productExists.length > 0) {
+    products = products.map((product) => {
+      if (product.id === newProduct.id) {
+        product.quantity = product.quantity + newProduct.quantity
+      }
+
+      return product
+    })
+  } else {
+    products.push(newProduct)
+  }
+
+  const productsUpdated = JSON.stringify(products)
+  storage.set(CART_STORAGE, productsUpdated)
+
+  return products
+}
+
+export function storageProductRemove(productId: string) {
+  const products = storageProductGetAll()
+
+  const productsUpdated = products.filter((product) => product.id !== productId)
+  storage.set(CART_STORAGE, JSON.stringify(productsUpdated))
+
+  return productsUpdated
+}
+
+export function storageIncreaseItemQuantity(itemId: string) {
+  const oldItems = storageProductGetAll()
+  const existingItemIndex = oldItems.findIndex((coffee) => coffee.id === itemId)
+
+  const updatedOrder = [...oldItems]
+
+  if (existingItemIndex !== -1) {
+    updatedOrder[existingItemIndex].quantity += 1
+
+    storage.set(CART_STORAGE, JSON.stringify(updatedOrder))
   }
 }
 
-export async function storageProductSave(newProduct: StorageCartProps) {
-  try {
-    let products = await storageProductGetAll()
+export function storageDecreaseItemQuantity(itemId: string) {
+  const oldItems = storageProductGetAll()
+  const existingItemIndex = oldItems.findIndex((coffee) => coffee.id === itemId)
 
-    const productExists = products.filter(
-      (product) => product.id === newProduct.id,
-    )
+  const updatedOrder = [...oldItems]
 
-    if (productExists.length > 0) {
-      products = products.map((product) => {
-        if (product.id === newProduct.id) {
-          product.quantity = product.quantity + newProduct.quantity
-        }
+  if (existingItemIndex !== -1) {
+    updatedOrder[existingItemIndex].quantity -= 1
 
-        return product
-      })
-    } else {
-      products.push(newProduct)
-    }
-
-    const productsUpdated = JSON.stringify(products)
-    await AsyncStorage.setItem(CART_STORAGE, productsUpdated)
-
-    return products
-  } catch (error) {
-    throw error
-  }
-}
-
-export async function storageProductRemove(productId: string) {
-  try {
-    const products = await storageProductGetAll()
-
-    const productsUpdated = products.filter(
-      (product) => product.id !== productId,
-    )
-    await AsyncStorage.setItem(CART_STORAGE, JSON.stringify(productsUpdated))
-
-    return productsUpdated
-  } catch (error) {
-    throw error
+    storage.set(CART_STORAGE, JSON.stringify(updatedOrder))
   }
 }
